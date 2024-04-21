fd65799aef05dfee362aa094f589043ceedd39e4
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-04-21 17:29:22 +0900

    ArticleControllerをupdateする処理を修正

--- sail/conduit-api/app/Http/Controllers/ArticleController.php
+++ sail/conduit-api/app/Http/Controllers/ArticleController.php
@@ -56,6 +56,7 @@ class ArticleController extends Controller
     public function update(UpdateArticleRequest $request, Article $article)
     {
         $validated = $request->validated();
+        $validated['slug'] = \Str::slug($validated['title']);
 
         $article->update($validated);
 

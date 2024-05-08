9f5f3f605da94d104b8e34eb27aa09478ad68b1a
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-05-09 04:17:21 +0900

    commentsを投稿日順に変更

--- sail/conduit-api/app/Http/Controllers/CommentController.php
+++ sail/conduit-api/app/Http/Controllers/CommentController.php
@@ -14,7 +14,7 @@ class CommentController extends Controller
     public function index(Request $request, $id)
     {
         $article = Article::where('slug', $id)->firstOrFail();
-        $comments = $article->comments;
+        $comments = $article->comments()->orderBy('created_at', 'desc')->get();
 
 
         return new CommentCollection($comments);

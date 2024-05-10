1277021cda47b7ca80e55c62e4bd5d304a803a86
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-05-11 01:54:26 +0900

    pgsqlに対応

--- sail/conduit-api/database/migrations/2024_04_19_002014_create_articles_table.php
+++ sail/conduit-api/database/migrations/2024_04_19_002014_create_articles_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,12 +12,11 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            // MySQLのみ
-            $table->string('title',255)->collation('utf8mb4_general_ci')->unique();
-            $table->string('description',255)->nullable(false);
+            $table->string('title', 255)->collation('utf8mb4_general_ci')->unique();
+            $table->string('description', 255)->nullable(false);
             $table->text('body')->nullable(false);
-            $table->foreignId('author_id')->constrained("users");
-            $table->string("slug")->unique()->nullable(false);
+            $table->foreignId('author_id')->constrained('users');
+            $table->string('slug')->unique()->nullable(false);
             $table->timestamps();
         });
     }
